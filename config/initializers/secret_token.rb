b70de335384ec865bc9bcbd4470b8698a68e1539
ketan-webonise <ketan@weboniselab.com>
2013-01-23 12:15:48 +0530

    New rails project weboworld created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weboworld::Application.config.secret_token = '0d723873c7160526abefb1c522e726daa4347f8b6156e471b37c551cb0770ed1d89d8b1170fc8d31034ea81df7535093eedda584e8bfbf1889c4e8599a71d4cc'
